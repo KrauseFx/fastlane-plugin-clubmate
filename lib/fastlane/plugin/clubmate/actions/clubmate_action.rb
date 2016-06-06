c29d2f41b618402279a2444e7717906b0f1636c3
KrauseFx <KrauseFx@gmail.com>
2016-06-05 22:06:59 -0700

    Added rubocop

--- lib/fastlane/plugin/clubmate/actions/clubmate_action.rb
+++ lib/fastlane/plugin/clubmate/actions/clubmate_action.rb
@@ -2,52 +2,52 @@ module Fastlane
   module Actions
     class ClubmateAction < Action
       def self.run(params)
-        puts %{                              
-                            `  .yy: .sy-`+hd- `/o`                              
-                       --` :ds-oo++://///+++/:osh+`.:/`                         
-                   `.` hds///+shdmmmmmmmmmysyhhhs+/++y/-/+.                     
-                   +ms::+shyo/:mmmmmmmmmmms   ``-/oyhs//oho `.`                 
-               .yho/:+hy+-`   :mmmmmmmmmmmm-        `:oyho:/hm.                 
-            -/-+s-:yy+.       ommmmmmmmmmmmy           `.+hy//:+o+              
-            +ms.:ys-`         hmmmmmmmmmmmmm-             `:yh+/yo              
-         ://o--ys-           `mmmmmmmmmmmmmmy                -yh/-oyy-          
-         ymo`+h:             .mmmmmmmmmmmmmmm-                `:hy:yh`          
-       ::o/`ss`              .mmmmmmmmmmmmmmms                  .yd:+dy`        
-       sm/`y+`               .mmmmmmmmmmmmmmmm//:-..     ``..-`  `od/+y.        
-     .:y/`y+          `.--:/+odddddddhhhyys+/:-..-::+oyhhdddho.    om:odh/      
-     :ms oo   `.-:+oyhdddhy+:..`````````..-:/oshddmmmmmmmds/.      `yd-ho.      
-    `od.:h`  -shmmmmmmmmmdddhhhysyyys:ohhhyydmmmmmmdhyo/-`          .dy:mds`    
-    .oo h/    `.-:/++ooosssshmm:.:mmo `...` :mmm+:-.`                +m-ys.     
-   `+d.:d`                 `odmy:/dh+.      oy:s:                    .mo/my/    
-   `-o ss                  o-.hhmmdmyo:/+-  /+:y-                     hh.m/`    
-  `+dy ho                  ://:.mmms//-`.`:  .:.     `  `. -``        sd`ds`    
-   `/s`m+                  `--`.mmm:`   `o-          : .`:`- /`       sm`y.     
-   .sh`ms                .+hmdhdmmmmhy: oo           /`-`-` `+        hd`ms`    
-    -h.md            `-/sdmmmmmmmmmmmmh.hy:`        `/ /.-. /.       `ms-s`     
-   `oh+ym:         `+hmmmmmmmmmmmmmmmmmmmmmy:       -: -.-.-s`       /m:od+     
-    `oh:mh`      `/hmmmmmmmmmmmmmmmmmmmmmmmmms.    .o. .`-..:       `dh.d/      
-    `-/+om+    `:hmmmmmmmmmmmmmmmmmmmmmmmmmmmmd:`..`/-..../o        om-shs`     
-     `yd:ym/  :ymmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmd:`  `   +hs`       +m++d-       
-      `.y:ym+smmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmh-       `ym:       +m+/y+:       
-       :hh:ommmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmy`         .s      `od/+ms         
-         `h+/dmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmo   ``.-:/:-.     -hh:+o+/         
-         .syo:ommmmmmmmmmmmmmmmmmmmmmmmmmmmmms  -hmmmm/      .odo/hs            
-            +do:smmmmmmmmmmmmmmmmmmmmmmmmmmmmh.+mmmmmmm:   .ods:sss/            
-            -:::+:odmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmo/ydo/smh               
-                ddo:/ymmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmy++yh++/               
-                :. ods//sdmmmmmmmmmmmmmmmmmmmmmmmmmmmds++/sdm+                  
-                   :+/:/ho++oyhmmmmmmmmmmmmmmmmdhso++ohmm/  .`                  
-                       :yo+/hyso+++++++++++++++o-/hmm:`-/.                      
-                            `so:.+mmh+/ymmo`-+h:   --                           
-                                  ``    ..}.yellow
+        puts %(
+                            `  .yy: .sy-`+hd- `/o`
+                       --` :ds-oo++://///+++/:osh+`.:/`
+                   `.` hds///+shdmmmmmmmmmysyhhhs+/++y/-/+.
+                   +ms::+shyo/:mmmmmmmmmmms   ``-/oyhs//oho `.`
+               .yho/:+hy+-`   :mmmmmmmmmmmm-        `:oyho:/hm.
+            -/-+s-:yy+.       ommmmmmmmmmmmy           `.+hy//:+o+
+            +ms.:ys-`         hmmmmmmmmmmmmm-             `:yh+/yo
+         ://o--ys-           `mmmmmmmmmmmmmmy                -yh/-oyy-
+         ymo`+h:             .mmmmmmmmmmmmmmm-                `:hy:yh`
+       ::o/`ss`              .mmmmmmmmmmmmmmms                  .yd:+dy`
+       sm/`y+`               .mmmmmmmmmmmmmmmm//:-..     ``..-`  `od/+y.
+     .:y/`y+          `.--:/+odddddddhhhyys+/:-..-::+oyhhdddho.    om:odh/
+     :ms oo   `.-:+oyhdddhy+:..`````````..-:/oshddmmmmmmmds/.      `yd-ho.
+    `od.:h`  -shmmmmmmmmmdddhhhysyyys:ohhhyydmmmmmmdhyo/-`          .dy:mds`
+    .oo h/    `.-:/++ooosssshmm:.:mmo `...` :mmm+:-.`                +m-ys.
+   `+d.:d`                 `odmy:/dh+.      oy:s:                    .mo/my/
+   `-o ss                  o-.hhmmdmyo:/+-  /+:y-                     hh.m/`
+  `+dy ho                  ://:.mmms//-`.`:  .:.     `  `. -``        sd`ds`
+   `/s`m+                  `--`.mmm:`   `o-          : .`:`- /`       sm`y.
+   .sh`ms                .+hmdhdmmmmhy: oo           /`-`-` `+        hd`ms`
+    -h.md            `-/sdmmmmmmmmmmmmh.hy:`        `/ /.-. /.       `ms-s`
+   `oh+ym:         `+hmmmmmmmmmmmmmmmmmmmmmy:       -: -.-.-s`       /m:od+
+    `oh:mh`      `/hmmmmmmmmmmmmmmmmmmmmmmmmms.    .o. .`-..:       `dh.d/
+    `-/+om+    `:hmmmmmmmmmmmmmmmmmmmmmmmmmmmmd:`..`/-..../o        om-shs`
+     `yd:ym/  :ymmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmd:`  `   +hs`       +m++d-
+      `.y:ym+smmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmh-       `ym:       +m+/y+:
+       :hh:ommmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmy`         .s      `od/+ms
+         `h+/dmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmo   ``.-:/:-.     -hh:+o+/
+         .syo:ommmmmmmmmmmmmmmmmmmmmmmmmmmmmms  -hmmmm/      .odo/hs
+            +do:smmmmmmmmmmmmmmmmmmmmmmmmmmmmh.+mmmmmmm:   .ods:sss/
+            -:::+:odmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmo/ydo/smh
+                ddo:/ymmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmy++yh++/
+                :. ods//sdmmmmmmmmmmmmmmmmmmmmmmmmmmmds++/sdm+
+                   :+/:/ho++oyhmmmmmmmmmmmmmmmmdhso++ohmm/  .`
+                       :yo+/hyso+++++++++++++++o-/hmm:`-/.
+                            `so:.+mmh+/ymmo`-+h:   --
+                                  ``    ..).yellow
       end
 
       def self.description
-        %q{Print a Club Mate in your build output}
+        'Print a Club Mate in your build output'
       end
 
       def self.authors
-        [%q{KrauseFx}]
+        ['KrauseFx']
       end
 
       def self.available_options
